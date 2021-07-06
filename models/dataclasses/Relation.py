1c99bdd458d4b55934e7b978b207fb1a477a2d8b
yulkino <yulkino@hotmail.com>
2021-07-07 01:58:13 +0500

    new realisation

--- models/dataclasses/Relation.py
+++ models/dataclasses/Relation.py
@@ -1,5 +1,6 @@
 class Relation:
-    def __init__(self, first, second, weight):
+    def __init__(self, first, second, weight, params_id):
         self.first = first
         self.second = second
         self.weight = weight
+        self.params_id = params_id
